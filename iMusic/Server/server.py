4554d3b81fe231183548734eed4ea05890ca612a
AndreiTeodorPop <andrei_teodor.pop@yahoo.com>
2026-04-20 17:28:24 +0300

    fix

--- iMusic/Server/server.py
+++ iMusic/Server/server.py
@@ -39,11 +39,6 @@ CACHE_TTL = 3600  # YouTube URLs expire in ~6h; refresh after 1h to be safe
 # Browser cookies work with web-based clients only; ios/android API endpoints
 # reject browser cookies and fail with "no player response".
 _CLIENT_PROFILES = [
-    (["mweb"],        True),
-    (["web"],         True),
-    (["tv_embedded"], True),
-    (["web"],         True),
-    (["ios"],         False),
     (["android"],     False),
 ]
 
